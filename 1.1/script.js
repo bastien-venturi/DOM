e762d5d011444c4b32b0fe90c5276d1d366438f6
Venturi <bastien.aristide.venturi@gmail.com>
2023-09-18 16:20:47 +0200

    start 3

--- 1.1/script.js
+++ 1.1/script.js
@@ -2,16 +2,16 @@ document.title = "Modifying the DOM";
 console.log(document.title);
 document.body.style.backgroundColor = "#FF69B4";
 
-function getRandomColor() {
+function Random() {
 let r = Math.floor(Math.random() * 256);
 let g = Math.floor(Math.random() * 256);
 let b = Math.floor(Math.random() * 256);
 return `rgb(${r},${g},${b})`;
 }
-document.body.style.backgroundColor = getRandomColor();
+document.body.style.backgroundColor = Random();
 
 
-let bodyElement = document.body.querySelector('main');
+let bodyElement = document.body;
 let secondChild = bodyElement.children[0];
 for (let child of secondChild.children) {
     console.log(child);
--- 1.2/script.js
+++ 1.2/script.js
@@ -1,5 +1,40 @@
 let importants = document.querySelectorAll('.important');
 importants.forEach(element => {
-    element.setAttribute('This is an important item');
+    element.setAttribute('title', 'This is an important item');
 });
 
+
+
+let imgTags = document.querySelectorAll('img');
+imgTags.forEach(function(img) {
+  if (!img.classList.contains('important')) {
+    img.style.display = 'none';
+  }
+});
+
+
+
+let paras = document.querySelectorAll('p');
+paras.forEach(function(para) {
+    let content = para.textContent;
+    if (para.classList.length > 0) {
+     console.log(`Class: ${para.className}, Content: ${content}`);
+    } else {
+    console.log(`Content: ${content}`);
+    }
+  });
+
+
+  function rand() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  paras.forEach(function(para) {
+    if (para.classList.length === 0) {
+      para.style.color = rand();
+    }
+  });
